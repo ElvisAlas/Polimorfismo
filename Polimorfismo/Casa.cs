4f07498754d1f723e38fc8eba3e85c82db8974f9
ElvisAlas <110005376+ElvisAlas@users.noreply.github.com>
2023-11-08 18:20:05 -0600

    test

---
+++ Polimorfismo/Casa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfismo
+{
+    internal class Casa : inmueble
+    {
+        public Casa(string tipo, double precio) { 
+        this.tipoInmueble = tipo;
+
+        }
+    }
+}
--- Polimorfismo/inmueble.cs
+++ Polimorfismo/inmueble.cs
@@ -9,5 +9,16 @@ namespace Polimorfismo
     internal class inmueble
     {
 
+        public string? tipoInmueble { get; set; }
+        public string? ubicacion {  get; set; }
+        public double areaTerreno { get; set; }
+        public double precio {  get; set; }
+
+        public void vender()
+        {
+            Console.WriteLine(tipoInmueble + "Vendida por el perecio de L. " + precio);
+
+        }
+
     }
 }
